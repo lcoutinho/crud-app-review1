3ea997c2821dd52e6e4bccba61009018c65f6347
Joe21 <joej21@gmail.com>
2014-01-23 22:43:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrudReview1::Application.config.secret_token = '546a8088982ded8fea30bd2ac31297b8bb974f9774d3dbb54dcb8c017c5bde908da76b4a8e0561ee5cde61403c579f7a851de020d8bdd41aed96fc471f337fbe'
