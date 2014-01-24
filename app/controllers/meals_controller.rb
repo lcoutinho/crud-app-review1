55c66ba9860ecd062cdf0ee768a92e68df556278
Joe21 <joej21@gmail.com>
2014-01-23 23:33:30 -0500

    Adding restful routing

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,6 +1,5 @@
 class MealsController < ApplicationController
 
 	def index
-		@first_meal = Meal.first
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 CrudReview1::Application.routes.draw do
-  root :to => "meals#index"
+  resources :meals
 end
+
+#     meals GET    /meals(.:format)          meals#index
+#           POST   /meals(.:format)          meals#create
+#  new_meal GET    /meals/new(.:format)      meals#new
+# edit_meal GET    /meals/:id/edit(.:format) meals#edit
+#      meal GET    /meals/:id(.:format)      meals#show
+#           PUT    /meals/:id(.:format)      meals#update
+#           DELETE /meals/:id(.:format)      meals#destroy
